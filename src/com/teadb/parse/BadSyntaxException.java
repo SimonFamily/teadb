fb622b0a272faaaa422e4d635904059e9df5bc31
wangyinhe <wyinhe@sina.cn>
2024-08-22 12:56:55 +0800

    增加sql解析模块

---
+++ src/com/teadb/parse/BadSyntaxException.java
@@ -0,0 +1,12 @@
+package com.teadb.parse;
+
+/**
+ * A runtime exception indicating that the submitted query
+ * has incorrect syntax.
+ */
+@SuppressWarnings("serial")
+public class BadSyntaxException extends RuntimeException {
+   public BadSyntaxException() {
+   }
+}
+
