95f79ff27d704f80b8c4f4dc7d2ec59a21bf1106
wangyinhe <wyinhe@sina.cn>
2024-08-25 12:35:49 +0800

    完善数据库服务

--- src/com/teadb/server/TeaDB.java
+++ src/com/teadb/server/TeaDB.java
@@ -1,61 +1,88 @@
 package com.teadb.server;
 
 import java.io.File;
-
-import com.teadb.buffer.BufferMgr;
 import com.teadb.file.FileMgr;
 import com.teadb.log.LogMgr;
+import com.teadb.buffer.BufferMgr;
 import com.teadb.tx.Transaction;
+import com.teadb.metadata.MetadataMgr;
+import com.teadb.plan.*;
 
+/**
+ * The class that configures the system.
+ */
 public class TeaDB {
-	public static int BLOCK_SIZE = 400;
-	public static int BUFFER_SIZE = 8;
-	public static String LOG_FILE = "teadb.log";
+   public static int BLOCK_SIZE = 400;
+   public static int BUFFER_SIZE = 8;
+   public static String LOG_FILE = "simpledb.log";
 
-	private FileMgr fm;
-	private BufferMgr bm;
-	private LogMgr lm;
+   private  FileMgr     fm;
+   private  BufferMgr   bm;
+   private  LogMgr      lm;
+   private  MetadataMgr mdm;
+   private  Planner planner;
 
-	/**
-	 * A constructor useful for debugging.
-	 * 
-	 * @param dirname   the name of the database directory
-	 * @param blocksize the block size
-	 * @param buffsize  the number of buffers
-	 */
-	public TeaDB(String dirname, int blocksize, int buffsize) {
-		File dbDirectory = new File(dirname);
-		fm = new FileMgr(dbDirectory, blocksize);
-		lm = new LogMgr(fm, LOG_FILE);
-		bm = new BufferMgr(fm, lm, buffsize);
-	}
+   /**
+    * A constructor useful for debugging.
+    * @param dirname the name of the database directory
+    * @param blocksize the block size
+    * @param buffsize the number of buffers
+    */
+   public TeaDB(String dirname, int blocksize, int buffsize) {
+      File dbDirectory = new File(dirname);
+      fm = new FileMgr(dbDirectory, blocksize);
+      lm = new LogMgr(fm, LOG_FILE);
+      bm = new BufferMgr(fm, lm, buffsize); 
+   }
+   
+   /**
+    * A simpler constructor for most situations. Unlike the
+    * 3-arg constructor, it also initializes the metadata tables.
+    * @param dirname the name of the database directory
+    */
+   public TeaDB(String dirname) {
+      this(dirname, BLOCK_SIZE, BUFFER_SIZE); 
+      Transaction tx = newTx();
+      boolean isnew = fm.isNew();
+      if (isnew)
+         System.out.println("creating new database");
+      else {
+         System.out.println("recovering existing database");
+         tx.recover();
+      }
+      mdm = new MetadataMgr(isnew, tx);
+      QueryPlanner qp = new BasicQueryPlanner(mdm);
+      UpdatePlanner up = new BasicUpdatePlanner(mdm);
+//    QueryPlanner qp = new HeuristicQueryPlanner(mdm);
+//    UpdatePlanner up = new IndexUpdatePlanner(mdm);
+      planner = new Planner(qp, up);
+      tx.commit();
+   }
+   
+   /**
+    * A convenient way for clients to create transactions
+    * and access the metadata.
+    */
+   public Transaction newTx() {
+      return new Transaction(fm, lm, bm);
+   }
+   
+   public MetadataMgr mdMgr() {
+      return mdm;
+   }
+   
+   public Planner planner() {
+      return planner;
+   }
 
-	/**
-	 * A simpler constructor for most situations. Unlike the 3-arg constructor, it
-	 * also initializes the metadata tables.
-	 * 
-	 * @param dirname the name of the database directory
-	 */
-	public TeaDB(String dirname) {
-		this(dirname, BLOCK_SIZE, BUFFER_SIZE);
-	}
-
-	/**
-	 * A convenient way for clients to create transactions and access the metadata.
-	 */
-	public Transaction newTx() {
-		return new Transaction(fm, lm, bm);
-	}
-
-	public FileMgr fileMgr() {
-		return fm;
-	}
-
-	public LogMgr logMgr() {
-		return lm;
-	}
-
-	public BufferMgr bufferMgr() {
-		return bm;
-	}
-}
+   // These methods aid in debugging
+   public FileMgr fileMgr() {
+      return fm;
+   }   
+   public LogMgr logMgr() {
+      return lm;
+   }   
+   public BufferMgr bufferMgr() {
+      return bm;
+   }   
+ }
