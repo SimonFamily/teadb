7f312ac1462fc8f057269c35d026cb65306c883a
wangyinhe <wyinhe@sina.cn>
2024-08-27 22:49:04 +0800

    索引完善

--- src/com/teadb/index/hash/HashIndex.java
+++ src/com/teadb/index/hash/HashIndex.java
@@ -5,15 +5,14 @@ import com.teadb.record.*;
 import com.teadb.index.Index;
 import com.teadb.query.*;
 
-
 /**
- * A static hash implementation of the Index interface.
- * A fixed number of buckets is allocated (currently, 100),
- * and each bucket is implemented as a file of index records.
+ * A static hash implementation of the Index interface. A fixed number of
+ * buckets is allocated (currently, 100), and each bucket is implemented as a
+ * file of index records.
  */
 public class HashIndex implements Index {
 	public static int NUM_BUCKETS = 100;
-   private Transaction tx;
+	private Transaction tx;
 	private String idxname;
 	private Layout layout;
 	private Constant searchkey = null;
@@ -21,23 +20,23 @@ public class HashIndex implements Index {
 
 	/**
 	 * Opens a hash index for the specified index.
+	 * 
 	 * @param idxname the name of the index
-	 * @param sch the schema of the index records
-	 * @param tx the calling transaction
+	 * @param sch     the schema of the index records
+	 * @param tx      the calling transaction
 	 */
 	public HashIndex(Transaction tx, String idxname, Layout layout) {
-      this.tx = tx;
+		this.tx = tx;
 		this.idxname = idxname;
 		this.layout = layout;
 	}
 
 	/**
-	 * Positions the index before the first index record
-	 * having the specified search key.
-	 * The method hashes the search key to determine the bucket,
-	 * and then opens a table scan on the file
-	 * corresponding to the bucket.
-	 * The table scan for the previous bucket (if any) is closed.
+	 * Positions the index before the first index record having the specified search
+	 * key. The method hashes the search key to determine the bucket, and then opens
+	 * a table scan on the file corresponding to the bucket. The table scan for the
+	 * previous bucket (if any) is closed.
+	 * 
 	 * @see simpledb.index.Index#beforeFirst(simpledb.query.Constant)
 	 */
 	public void beforeFirst(Constant searchkey) {
@@ -49,10 +48,10 @@ public class HashIndex implements Index {
 	}
 
 	/**
-	 * Moves to the next record having the search key.
-	 * The method loops through the table scan for the bucket,
-	 * looking for a matching record, and returning false
+	 * Moves to the next record having the search key. The method loops through the
+	 * table scan for the bucket, looking for a matching record, and returning false
 	 * if there are no more such records.
+	 * 
 	 * @see simpledb.index.Index#next()
 	 */
 	public boolean next() {
@@ -63,8 +62,9 @@ public class HashIndex implements Index {
 	}
 
 	/**
-	 * Retrieves the dataRID from the current record
-	 * in the table scan for the bucket.
+	 * Retrieves the dataRID from the current record in the table scan for the
+	 * bucket.
+	 * 
 	 * @see simpledb.index.Index#getDataRid()
 	 */
 	public RID getDataRid() {
@@ -75,7 +75,9 @@ public class HashIndex implements Index {
 
 	/**
 	 * Inserts a new record into the table scan for the bucket.
-	 * @see simpledb.index.Index#insert(simpledb.query.Constant, simpledb.record.RID)
+	 * 
+	 * @see simpledb.index.Index#insert(simpledb.query.Constant,
+	 *      simpledb.record.RID)
 	 */
 	public void insert(Constant val, RID rid) {
 		beforeFirst(val);
@@ -86,15 +88,16 @@ public class HashIndex implements Index {
 	}
 
 	/**
-	 * Deletes the specified record from the table scan for
-	 * the bucket.  The method starts at the beginning of the
-	 * scan, and loops through the records until the
+	 * Deletes the specified record from the table scan for the bucket. The method
+	 * starts at the beginning of the scan, and loops through the records until the
 	 * specified record is found.
-	 * @see simpledb.index.Index#delete(simpledb.query.Constant, simpledb.record.RID)
+	 * 
+	 * @see simpledb.index.Index#delete(simpledb.query.Constant,
+	 *      simpledb.record.RID)
 	 */
 	public void delete(Constant val, RID rid) {
 		beforeFirst(val);
-		while(next())
+		while (next())
 			if (getDataRid().equals(rid)) {
 				ts.delete();
 				return;
@@ -103,6 +106,7 @@ public class HashIndex implements Index {
 
 	/**
 	 * Closes the index by closing the current table scan.
+	 * 
 	 * @see simpledb.index.Index#close()
 	 */
 	public void close() {
@@ -111,16 +115,15 @@ public class HashIndex implements Index {
 	}
 
 	/**
-	 * Returns the cost of searching an index file having the
-	 * specified number of blocks.
-	 * The method assumes that all buckets are about the
-	 * same size, and so the cost is simply the size of
-	 * the bucket.
+	 * Returns the cost of searching an index file having the specified number of
+	 * blocks. The method assumes that all buckets are about the same size, and so
+	 * the cost is simply the size of the bucket.
+	 * 
 	 * @param numblocks the number of blocks of index records
-	 * @param rpb the number of records per block (not used here)
+	 * @param rpb       the number of records per block (not used here)
 	 * @return the cost of traversing the index
 	 */
-	public static int searchCost(int numblocks, int rpb){
+	public static int searchCost(int numblocks, int rpb) {
 		return numblocks / HashIndex.NUM_BUCKETS;
 	}
 }
